8dece1684bdf8382afe7e1f7b8821bf6caf5f4d2
BayronSoto-19 <b.soto54@alumnos.santotomas.cl>
2023-10-26 16:52:02 -0300

    Hello worl 3

--- app/src/main/java/com/example/semana_10_git/MainActivity.java
+++ app/src/main/java/com/example/semana_10_git/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("HEllo MUNDO!!");
+        System.out.println("HEllo MUNDO!!");
+        System.out.println("HEllo MUNDO!!");
     }
 }
