0bf060813a9b4464d389b457e1a5d73bba696dbb
BayronSoto-19 <b.soto54@alumnos.santotomas.cl>
2023-10-26 17:00:46 -0300

    Cara feliz

--- app/src/main/java/com/example/semana_10_git/MainActivity.java
+++ app/src/main/java/com/example/semana_10_git/MainActivity.java
@@ -9,9 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("HEllo MUNDO!!");
-        System.out.println("HEllo MUNDO!!");
-        System.out.println("HEllo MUNDO!!");
+        // :)
     }
 }
